d62d5fee8ec754234c9552da72da16f9a7f0b8c3
Carter Cole <carter.cole@gmail.com>
2012-05-06 22:45:16 -0500

    changed names to ArduinoMotionPlus

--- ArduinoMotionPlus.h
+++ ArduinoMotionPlus.h
@@ -1,19 +1,14 @@
 /*
- * ArduinoNunchuk.h - Improved Wii Nunchuk library for Arduino
+ * ArduinoMotionPlus.h - Improved Wii Motion Plus library for Arduino
  * 
- * Copyright 2011-2012 Gabriel Bianconi, http://www.gabrielbianconi.com/
+ * Copyright 2011-2012 Carter Cole, http://www.cartercole.com/
  *
- * Project URL: http://www.gabrielbianconi.com/projects/arduinonunchuk/
  *
  * Based on the following projects/websites:
- *   http://www.windmeadow.com/node/42
- *   http://todbot.com/blog/2008/02/18/wiichuck-wii-nunchuck-adapter-available/
- *   http://wiibrew.org/wiki/Wiimote/Extension_Controllers
- * 
+ * Project URL: http://www.gabrielbianconi.com/projects/arduinonunchuk/
  */
-
-#ifndef ArduinoNunchuk_H
-#define ArduinoNunchuk_H
+#ifndef ArduinoMotionPlus_H
+#define ArduinoMotionPlus_H
 
 #if (ARDUINO >= 100)
   #include <Arduino.h>
@@ -21,7 +16,7 @@
   #include <WProgram.h>
 #endif
 
-class ArduinoNunchuk
+class ArduinoMotionPlus
 {  
   public:  
     int analogX;
